e1b89073028468f5012ff74838de5cfc1bfa2834
Emerson639 <202603621901@alunos.estacio.br>
2026-05-24 15:30:59 +0000

    Segunda codificacao no super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,19 +6,45 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  char estado;
-  char codigo-da-carta;
-  nome-da-cidade;
-  populacao;
-  area-cidade;
-  pib
-  numero-de-pontos-turisticos;
+  //char estado[1];
+  //char codigo[3];
+  //char nome[20];
+  //int populacao, numeropontosturisticos;
+  //float area;
+  //float pib;
+  
 
   // Área para entrada de dados
+  printf("Digite o Estado com apenas uma letra de A a H: \n");
+  //scanf("%c", &estado);
+
+  //printf("Digite o código do Estado de '01' a '04': \n");
+  //scanf("%s", &codigo);
+
+  //printf("Digite o nome da cidade: \n");
+  //scanf("%s", &nome);
+
+  //printf("Digite o total da população: \n");
+  //scanf("%d", &populacao); 
+
+  //printf("Digite a Área em km2: \n");
+  //scanf("%f", &area);
+
+  //printf("Digite o PIB: \n");
+  //scanf("%f", &pib); 
+
+  //prinf("Digite o numero de pontos turisticos: \n");
+  //scanf("%d", &numeropontosturisticos);
+
 
   // Área para exibição dos dados da cidade
-    printf("Olá Mundo, no Codespace!\n");
-    printf("Exemplo de edição de código!\n");
+  //  printf("O Estado é: %c\n", estado);
+  //  printf("Código do Estado é: %s\n", codigo);
+  //  printf("O nome da cidade é: %s\n", nome);
+  //  printf("O total da população é: %d\n", populacao);
+  //  printf("O tamanho da área em km2 é: %f\n", area);
+  //  printf("O PIB está em: %f\n", pib);
+  //  printf("O número de pontos turísticos é: %d\n", numeropontosturisticos);
 
 return 0;
 } 
