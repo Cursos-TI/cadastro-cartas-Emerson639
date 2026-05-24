5b7d37e5ec3eb642686241393ec01e96d8daa813
Emerson639 <202603621901@alunos.estacio.br>
2026-05-24 17:44:40 +0000

    Codificação final da carta número 01!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h> // Necessário para usar a função strcspn
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
@@ -6,45 +7,59 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  //char estado[1];
-  //char codigo[3];
-  //char nome[20];
-  //int populacao, numeropontosturisticos;
-  //float area;
-  //float pib;
+  char estado;
+  char codigo[4];
+  char nome[50];
+  int populacao, numeropontosturisticos;
+  float area;
+  float pib;
   
 
   // Área para entrada de dados
-  printf("Digite o Estado com apenas uma letra de A a H: \n");
-  //scanf("%c", &estado);
+  printf("Digite o Estado com apenas uma letra de 'A' a 'H': \n");
+  scanf(" %c", &estado);
 
-  //printf("Digite o código do Estado de '01' a '04': \n");
-  //scanf("%s", &codigo);
+  printf("Digite o código do Estado de '01' a '04': \n");
+  scanf("%s", codigo);
 
-  //printf("Digite o nome da cidade: \n");
-  //scanf("%s", &nome);
+  getchar() != '\n'; // Necessário para digitar o nome da cidade, senão o programa pula esse bloco.
 
-  //printf("Digite o total da população: \n");
-  //scanf("%d", &populacao); 
+  printf("Digite o nome da cidade: \n");
+  fgets(nome, sizeof(nome), stdin);
+  nome[strcspn(nome, "\n")] = '\0';
+  
+  printf("Digite o total da população: \n");
+  scanf("%d", &populacao); 
 
-  //printf("Digite a Área em km2: \n");
-  //scanf("%f", &area);
+  printf("Digite a Área em km2: \n");
+  scanf("%f", &area);
 
-  //printf("Digite o PIB: \n");
-  //scanf("%f", &pib); 
+  printf("Digite o PIB: \n");
+  scanf("%f", &pib); 
 
   //prinf("Digite o numero de pontos turisticos: \n");
   //scanf("%d", &numeropontosturisticos);
 
 
   // Área para exibição dos dados da cidade
-  //  printf("O Estado é: %c\n", estado);
-  //  printf("Código do Estado é: %s\n", codigo);
-  //  printf("O nome da cidade é: %s\n", nome);
-  //  printf("O total da população é: %d\n", populacao);
-  //  printf("O tamanho da área em km2 é: %f\n", area);
-  //  printf("O PIB está em: %f\n", pib);
+  printf("##############################################################\n");
+  printf("################   Dados da carta número 01   ################\n");
+  printf("##############################################################\n");
+  printf("\n");
+
+    printf("O Estado é: %c\n", estado);
+    printf("Código do Estado é: %s\n", codigo);
+    printf("O nome da cidade é: %s\n", nome);
+    printf("O total da população é: %d\n", populacao);
+    printf("O tamanho da área em km2 é: %.2f\n", area);
+    printf("O PIB está em: %.2f bilhões de reais\n", pib);
   //  printf("O número de pontos turísticos é: %d\n", numeropontosturisticos);
+
+
+  printf("\n");
+  printf("##############################################################\n");
+  printf("################   Fim carta número 01   #####################\n");
+  printf("##############################################################\n");
 
 return 0;
 } 
